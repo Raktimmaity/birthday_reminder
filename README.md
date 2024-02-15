5074bce70974a9898a2dd57d2213d7922d29d8b4
Raktimmaity <raktimmaity2003@gmail.com>
2024-02-15 17:55:59 +0530

    birthday reminder

---
+++ README.md
@@ -0,0 +1,16 @@
+# Birthday Reminder
+
+# Features
+
+- Interactive UI
+- Create using React JS + vite
+
+
+## Authors
+
+- [@Raktimmaity](https://github.com/Raktimmaity)
+
+
+## Screenshots
+
+![App Screenshot](./birthday.png)
---
+++ src/data.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: 'Raktim Maity',
+    age: 21,
+    image:
+      'https://images.pexels.com/photos/1222271/pexels-photo-1222271.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    id: 2,
+    name: 'John',
+    age: 32,
+    image:
+      'https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+  {
+    id: 3,
+    name: 'Riju',
+    age: 36,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+  },
+  {
+    id: 4,
+    name: 'Peter',
+    age: 34,
+    image:
+      'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+  },
+  {
+    id: 5,
+    name: 'Sagar',
+    age: 29,
+    image:
+      'https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+  },
+];
